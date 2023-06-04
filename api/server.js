1a35af567413f9804963a8a38da5447ef7b4d2c3
juanxbini <juanxbini@gmail.com>
2023-06-04 20:38:34 -0300

    commit

--- api/server.js
+++ api/server.js
@@ -1,14 +1,14 @@
 // api/server.js
 
 import express from 'express';
-import Router from './routes/indexRoute.js'
+import Router from './routes/indexRoute.js';
 
 // Crea una instancia de Express
 const app = express();
 // Configura el puerto
 const port = 3000;
 
-// Configura las rutas
+// Configura las rutas 
 // ...
 app.use('/api', Router); // Usa el router de las rutas configuradas en el archivo index.js
 
