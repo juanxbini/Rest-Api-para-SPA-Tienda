11343dfaff17052f3e89831a0e5241099c91d50f
juanxbini <juanxbini@gmail.com>
2023-06-05 23:09:11 -0300

    borramos la configuracion de cors

--- api/server.js
+++ api/server.js
@@ -13,12 +13,7 @@ const port = 3000;
 app.use('/api', Router); // Usa el router de las rutas configuradas en el archivo index.js
 
 //cors 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:80");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-    res.header("Access-Control-Allow-Headers", "Content-Type");
-    next();
-  });
+
 // Iniciar el servidor
 app.listen(port, () => {
     console.log(`Servidor iniciado en el puerto ${port}`);
