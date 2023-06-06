a02aa9bb55ab5e23a54528867f06739cfc05cab9
juanxbini <juanxbini@gmail.com>
2023-06-06 01:09:34 -0300

    configuramos cors

--- api/server.js
+++ api/server.js
@@ -2,17 +2,17 @@
 
 import express from 'express';
 import Router from './routes/indexRoute.js';
-
+import cors from 'cors'
 // Crea una instancia de Express
 const app = express();
 // Configura el puerto
 const port = 3000;
 
+//cors 
+app.use(cors())
 // Configura las rutas 
 // ...
 app.use('/api', Router); // Usa el router de las rutas configuradas en el archivo index.js
-
-//cors 
 
 // Iniciar el servidor
 app.listen(port, () => {
